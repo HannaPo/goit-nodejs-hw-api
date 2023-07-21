dcc4ca0514708e1bf46c82ccb13b95d0c35cb3dd
HannaPo <114150796+HannaPo@users.noreply.github.com>
2023-07-21 15:16:52 +0300

    connection to mongodb

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ dotenv.config();
 
 const { DB_HOST, PORT } = process.env;
 
-mongoose.connect(DB_HOST)
+mongoose
+  .connect(DB_HOST)
   .then(() => {
     console.log(`Database connection successful`);
     app.listen(PORT, () => {
-      console.log(`Server running. Use our API on port: ${PORT}`)
-    })
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
   })
   .catch(error => {
     console.error(`Database connection error: ${error.message}`);
