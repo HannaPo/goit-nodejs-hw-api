91af247ee7c2921218ae64142fd2a075d1627690
HannaPo <114150796+HannaPo@users.noreply.github.com>
2023-07-23 17:02:12 +0300

    done

--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -1,19 +1,33 @@
 import express from 'express';
 
 import ctrl from '../../controllers/contacts-controller.js';
-import contactsSchema from '../../schemas/contactsSchema.js';
-import { isEmptyBody, validateBody } from '../../middlewares/index.js';
+import contactsSchemas from '../../schemas/contactsSchema.js';
+import { isEmptyBody, validateBody, isValidId } from '../../middlewares/index.js';
 
 const contactsRouter = express.Router();
 
 contactsRouter.get('/', ctrl.getAllContacts);
 
-contactsRouter.get('/:contactId', ctrl.getById);
+contactsRouter.get('/:contactId', isValidId, ctrl.getById);
 
-contactsRouter.delete('/:contactId', ctrl.deleteById);
+contactsRouter.post('/', isEmptyBody, validateBody(contactsSchemas.contactsSchema), ctrl.addContact);
 
-contactsRouter.post('/', isEmptyBody, validateBody(contactsSchema), ctrl.addContact);
+contactsRouter.put(
+  '/:contactId',
+  isValidId,
+  isEmptyBody,
+  validateBody(contactsSchemas.contactsSchema),
+  ctrl.updateById
+);
 
-contactsRouter.put('/:contactId', isEmptyBody, validateBody(contactsSchema), ctrl.updateById);
+contactsRouter.patch(
+  '/:contactId/favorite',
+  isValidId,
+  isEmptyBody,
+  validateBody(contactsSchemas.contactUpdateFavoriteSchema),
+  ctrl.updateStatusContact
+);
+
+contactsRouter.delete('/:contactId', isValidId, ctrl.deleteById);
 
 export default contactsRouter;
--- schemas/contactsSchema.js
+++ schemas/contactsSchema.js
@@ -4,15 +4,22 @@ const contactsSchema = Joi.object({
   name: Joi.string().alphanum().min(3).max(30).required().messages({
     'any.required': 'missing required name field',
   }),
-  email: Joi.string()
-    .email()
+  email: Joi.string().email().required().messages({
+    'any.required': 'missing required email field',
+  }),
+  phone: Joi.string()
+    .regex(/^[0-9 ()+-]+$/)
     .required()
     .messages({
-      'any.required': 'missing required email field',
+      'any.required': 'missing required phone field',
     }),
-  phone: Joi.string().required().messages({
-    'any.required': 'missing required phone field',
+  favorite: Joi.boolean(),
+});
+
+const contactUpdateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required().messages({
+    'any.required': 'missing field favorite',
   }),
 });
 
-export default contactsSchema;
+export default { contactsSchema, contactUpdateFavoriteSchema };
